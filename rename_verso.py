0c0c121acee3a3d8d8fe1fc12360e5450149314b
Feilong <feilong.dragona@gmail.com>
2018-07-15 11:31:16 +0800

    DCP does not provide a recto-verso instant scan. Each size needs to be scanned independently and compiling the book into a single pdf file requires file renaming. The following two scripts help in renaming the files in batch.

---
+++ rename_verso.py
@@ -0,0 +1,5 @@
+import os
+list_file = os.listdir()
+files = [file_name for file_name in list_file if file_name.find('pdf') != -1]
+numbers = [element for element in range(2, (len(files)*2)+1, 2)]
+[os.rename(file_name, "exploring_english_"+str(numbers[index])+".pdf") for index, file_name in enumerate(files)]
